dd6efba5711caf476ccc01bea9954582ddc8d14b
MartinArj <martin47412@gmail.com>
2023-11-21 16:37:31 +0530

    get function created

--- union_opperation/Program.cs
+++ union_opperation/Program.cs
@@ -9,7 +9,18 @@ namespace union_opperation
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+        
         }
+        public static int[] get(int n)
+        {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+         
     }
 }
