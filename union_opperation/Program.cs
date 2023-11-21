b8324e9cb1c6c9424cf06dfad58e7dd7b6344a9c
MartinArj <martin47412@gmail.com>
2023-11-21 16:55:32 +0530

    union fuction created

--- union_opperation/Program.cs
+++ union_opperation/Program.cs
@@ -21,6 +21,44 @@ namespace union_opperation
             }
             return a;
         }
-         
+        public static int union()
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[m] ;
+            int[] b = new int[n] ;
+            a = get(m);
+            b = get(n);
+            int count = 0;
+            int j = 0;
+            for (int i = 0; i < a.Length + b.Length; i++)
+            {
+               
+
+                for (
+                    j = 0; j < a.Length + b.Length; j++)
+                {
+                    if (i < a.Length)
+                    {
+                        if (a[i] == a[j])
+                            break;
+                    }
+                    if (i >= a.Length)
+                    {
+                        if (j < a.Length && a[j] == b[i - a.Length])
+                            break;
+                        if (j >= a.Length && b[i - a.Length] == b[j - a.Length])
+                            break;
+                    }
+
+                }
+                if (i == j)
+                    count++;
+
+
+            }
+            return count;
+
+        }
     }
 }
